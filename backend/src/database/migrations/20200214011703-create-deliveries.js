5d89bd14c420c2a8a829a501aca66ab1b48ebb65
C1311596 Wallyson Galvao Da Cunha <c1311596@interno.bb.com.br>
2020-03-04 18:47:09 -0300

    criacao das migrations

--- backend/src/database/migrations/20200214011703-create-deliveries.js
+++ backend/src/database/migrations/20200214011703-create-deliveries.js
@@ -1,23 +1,61 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('deliveries', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            recipient_id: {
+                type: Sequelize.INTEGER,
+                references: { model: 'recipients', key: 'id' },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+                allowNull: false,
+            },
+            deliveryman_id: {
+                type: Sequelize.INTEGER,
+                references: { model: 'deliverymen', key: 'id' },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+                allowNull: false,
+            },
+            signature_id: {
+                type: Sequelize.INTEGER,
+                references: { model: 'files', key: 'id' },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+                allowNull: true,
+            },
+            product: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            canceled_at: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            start_date: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            end_date: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+    down: queryInterface => {
+        return queryInterface.dropTable('deliveries');
+    },
 };
