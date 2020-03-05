1e933dfbb39b9a6856ed096bb8548b8d5a7d3fe2
Galvao Da Cunha <wgalvao@indracompany.com>
2020-03-04 22:17:14 -0300

    rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,12 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 import FileController from './app/controllers/FileController';
+import DeliveryController from './app/controllers/DeliveryController';
+import DeliveryStatusController from './app/controllers/DeliveryStatusController';
+import DeliveryProblemController from './app/controllers/DeliveryProblemController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,8 +20,35 @@ routes.post('/users', UserController.store);
 
 routes.post('/session', SessionController.store);
 
+routes.get('/deliveryman/deliveries', DeliveryStatusController.index);
+// Listar encomendas finalizadas
+routes.get('/deliveryman/:id/deliveries', DeliveryStatusController.show);
+
+routes.put(
+    '/deliveryman/:deliveryman_id/deliveries/:delivery_id',
+    DeliveryStatusController.update
+);
+routes.post('/files/signature', upload.single('file'), FileController.store);
+routes.post('/deliveries/:id/problems', DeliveryProblemController.store);
+
 routes.use(authMiddleware);
 
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/deliverymans', DeliverymanController.store);
+routes.get('/deliverymans', DeliverymanController.index);
+routes.put('/deliverymans/:id', DeliverymanController.update);
+routes.delete('/deliverymans/:id', DeliverymanController.delete);
+
+routes.post('/deliveries', DeliveryController.store);
+routes.get('/deliveries', DeliveryController.index);
+routes.put('/deliveries/:id', DeliveryController.update);
+routes.delete('/deliveries/:id', DeliveryController.delete);
+
+routes.get('/deliveries/:id/problems', DeliveryProblemController.index);
+routes.delete('/problem/:id/cancel-delivery', DeliveryProblemController.delete);
 
 export default routes;
