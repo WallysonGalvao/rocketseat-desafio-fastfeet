6eff3946f0dcb17730ddf8b552ce5e2672896a4c
C1311596 Wallyson Galvao Da Cunha <c1311596@interno.bb.com.br>
2020-03-11 21:42:50 -0300

    pagina de pedidos

---
+++ frontend/src/components/ContentContainer/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    margin: 20px auto;
+    width: 95%;
+    max-width: 1200px;
+`;
---
+++ frontend/src/components/PageButton/styles.js
@@ -0,0 +1,28 @@
+import { darken } from 'polished';
+import styled from 'styled-components';
+
+import { colors } from '~/components/colors';
+
+export const Button = styled.button`
+    color: #fff;
+    align-items: center;
+    padding: 9px;
+    background: ${colors.primary};
+    border-radius: 4px;
+    border: none;
+    &:first-child {
+        margin-right: 15px;
+    }
+    span {
+        font-size: 1.4rem;
+        font-weight: bold;
+        color: #fff;
+    }
+    &:hover {
+        background: ${darken(0.1, colors.primary)};
+    }
+    &:disabled {
+        cursor: not-allowed;
+        background: #666;
+    }
+`;
